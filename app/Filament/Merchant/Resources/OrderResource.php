22e0c05a02f6c226394dc04537c40059c245704b
Lucifer0885 <georgeskotidismain@gmail.com>
2025-06-07 02:51:26 +0300

    fix: remove none existant view page in order resource

--- app/Filament/Merchant/Resources/OrderResource.php
+++ app/Filament/Merchant/Resources/OrderResource.php
@@ -388,6 +388,7 @@ class OrderResource extends Resource
             'index' => Pages\ListOrders::route('/'),
             'create' => Pages\CreateOrder::route('/create'),
             'edit' => Pages\EditOrder::route('/{record}/edit'),
+
         ];
     }
 }
